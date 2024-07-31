f83c619a28a971d19807686a90d6eaa7741b11d3
Maria Skhabitskaya <maria.skhabitskaya@gmail.com>
2024-07-31 23:04:07 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+void Main ()
+{
+    int M = ReadInt("Введите первое значение: ");
+    int N = ReadInt("Введите второе значение: ");
+
+    if (M >= N)
+    {
+        System.Console.WriteLine("Первое значение должно быть больше второго");
+        return; 
+    }
+        
+    
+    ShowDigits(M, N);
+}
+
+void ShowDigits (int M, int N)
+{
+    if (M > N) return;
+
+    else if (M < N)
+    {
+        System.Console.Write(M + ", ");
+        ShowDigits(M + 1, N);   
+    }
+
+    else 
+    {
+        System.Console.Write(M);
+        ShowDigits(M + 1, N); 
+    }
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
